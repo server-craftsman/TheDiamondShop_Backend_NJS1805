10f0b1b2b2a02dc6151c6880a1e5660ecefd7729
kaitou141 <hdk18112003@gmail.com>
2024-06-30 15:01:11 +0700

    Update WarrantyDAO

--- dao/warranty/warrantyDAO.js
+++ dao/warranty/warrantyDAO.js
@@ -65,6 +65,7 @@ const createWarranty = async (warrantyData) => {
 const updateWarranty = async (warrantyData) => {
     try {
         const {
+            reportNo,
             description,
             date,
             placeToBuy,
@@ -78,35 +79,35 @@ const updateWarranty = async (warrantyData) => {
 
         let pool = await sql.connect(config);
         let results = await pool
-        .request()
-        .input("ReportNo", sql.VarChar, reportNo)
-        .input("Description", sql.VarChar, description)
-        .input("Date", sql.Date, date)
-        .input("PlaceToBuy", sql.VarChar, placeToBuy)
-        .input("Period", sql.Date, period)
-        .input("WarrantyType", sql.VarChar, warrantyType)
-        .input("WarrantyConditions", sql.VarChar, warrantyConditions)
-        .input("AccompaniedService", sql.VarChar, accompaniedService)
-        .input("Condition", sql.VarChar, condition)
-        .input("OrderDetailID", sql.Int, orderDetailId)
-        .query(`UPDATE WarrantyReceipt
-                SET
-                    Description = @Description,
-                    Date = @Date,
-                    PlaceToBuy = @PlaceToBuy,
-                    Period = @Period,
-                    WarrantyType = @WarrantyType,
-                    WarrantyConditions = @WarrantyConditions,
-                    AccompaniedService = @AccompaniedService,
-                    Condition = @Condition
-                    OrderDetailId = @OrderDetailID
-                WHERE ReportNo = @ReportNo
+            .request()
+            .input("ReportNo", sql.VarChar, reportNo)
+            .input("Description", sql.VarChar, description)
+            .input("Date", sql.Date, date)
+            .input("PlaceToBuy", sql.VarChar, placeToBuy)
+            .input("Period", sql.Date, period)
+            .input("WarrantyType", sql.VarChar, warrantyType)
+            .input("WarrantyConditions", sql.VarChar, warrantyConditions)
+            .input("AccompaniedService", sql.VarChar, accompaniedService)
+            .input("Condition", sql.VarChar, condition)
+            .input("OrderDetailID", sql.Int, orderDetailId)
+            .query(`UPDATE WarrantyReceipt
+                    SET
+                        Descriptions = @Description,
+                        Date = @Date,
+                        PlaceToBuy = @PlaceToBuy,
+                        Period = @Period,
+                        WarrantyType = @WarrantyType,
+                        WarrantyConditions = @WarrantyConditions,
+                        AccompaniedService = @AccompaniedService,
+                        Condition = @Condition,
+                        OrderDetailId = @OrderDetailID
+                    WHERE ReportNo = @ReportNo
                 `);
         return results;
-    }catch (err) {
+    } catch (err) {
         console.error("Database query error:", err);
         throw new Error("Database query error");
-      }
+    }
 };
 module.exports = {
     getWarranty,
